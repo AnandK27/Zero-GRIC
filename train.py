71013fa7d3992bbb94c6420779b71aa0cbdfba4a
Anand Kumar <63339285+AnandK27@users.noreply.github.com>
2024-06-03 14:14:15 -0700

    chore: Load weights if available in train.py

--- train.py
+++ train.py
@@ -96,6 +96,10 @@ if __name__ == '__main__':
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
+    #load weights
+    if os.path.exists(save_path + 'best_model.pt'):
+        model.load_state_dict(torch.load(save_path + 'best_model.pt'))
+
     model.train()
 
     best_loss = 1000
