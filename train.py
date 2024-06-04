06dbdb195dcc0d378f61322d301853023906aa16
Anand Kumar <63339285+AnandK27@users.noreply.github.com>
2024-06-04 15:35:24 -0700

    Refactor train.py to use lazy loading for image loading and fix indexing issue in generating neighbor captions

--- train.py
+++ train.py
@@ -167,14 +167,17 @@ class Blip2Retreiver(nn.Module):
         for i in range(k):
             edge_index.append([i, 0])
         edge_index = np.array(edge_index)
+        edge_indices = []
+        for i in range(batch_size):
+            edge_indices.append(edge_index + i*k)
+        edge_index = np.concatenate(edge_indices)
         edge_index = torch.tensor(edge_index, dtype=torch.long, device=self.device).T
-        #repeat for batch size
-        edge_index = edge_index.repeat(global_image_embeds.shape[0], 1, 1).permute(0, 2, 1)
-        edge_attr = scores.repeat(1,2).to(self.device)
+
+        edge_attr = scores.repeat(1,2).to(self.device).flatten()
             
-        x = self.graph_conv1(x, edge_index.cpu(), edge_attr)
+        x = self.graph_conv1(x, edge_index, edge_attr)
         x = torch.relu(x)
-        x = self.graph_conv2(x, edge_index.cpu(), edge_attr)
+        x = self.graph_conv2(x, edge_index, edge_attr)
         x = torch.relu(x)
 
         image_embeds[:, 0, :] = x[:, 0, :]
