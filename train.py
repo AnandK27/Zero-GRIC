067743d411ceb3699eebc900518d204f9ec023a5
Anand Kumar <63339285+AnandK27@users.noreply.github.com>
2024-06-04 15:48:19 -0700

    Refactor train.py to use lazy loading for image loading and remove unnecessary print statements

--- train.py
+++ train.py
@@ -61,8 +61,6 @@ class Blip2Retreiver(nn.Module):
         edge_index = torch.tensor(edge_index, dtype=torch.long, device=self.device).T
 
         edge_attr = scores.repeat(1,2).to(self.device).flatten()
-        print(edge_attr.shape)
-        print(edge_index.shape)
             
         x = self.graph_conv1(x.flatten(end_dim=-2), edge_index, edge_attr)
         x = torch.relu(x)
@@ -180,9 +178,6 @@ class Blip2Retreiver(nn.Module):
         edge_index = torch.tensor(edge_index, dtype=torch.long, device=self.device).T
 
         edge_attr = scores.repeat(1,2).to(self.device).flatten()
-        print(edge_attr.shape)
-        print(edge_index.shape)
-            
         x = self.graph_conv1(x.flatten(end_dim=-2), edge_index, edge_attr)
         x = torch.relu(x)
 
